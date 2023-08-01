9b572feabc836289be8492ae7b6d8954f1657302
Omar Badr <omarbadrh.ob@gmail.com>
2023-08-01 17:23:03 +0200

    :lipstick: create-room page is finished, some minor fixes added

--- base/form.py
+++ base/form.py
@@ -1,10 +1,16 @@
-from django.forms import ModelForm
+from django.forms import ModelForm, Textarea
 from .models import Room, Message
+
 
 class RoomForm(ModelForm):
     class Meta:
         model = Room
         fields = '__all__'
+        exclude = ['participants']
+        # widgits = {
+        #     'description': Textarea(attrs={ 'rows': 5})
+        # }
+
 
 class MessageForm(ModelForm):
     class Meta:
