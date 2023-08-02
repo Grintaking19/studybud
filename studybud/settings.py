71f6d0f6c7fc6f480b8db1b3ac8ed5471f982ffd
Omar Badr <omarbadrh.ob@gmail.com>
2023-08-02 23:05:49 +0200

    :Bug: Fixing the cors error

--- studybud/settings.py
+++ studybud/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "base.apps.BaseConfig",
     'rest_framework',
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
@@ -49,6 +50,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
 ]
 
 ROOT_URLCONF = "studybud.urls"
@@ -130,3 +132,6 @@ STATICFILES_DIRS = [
  ]
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+CORS_ALLOW_ALL_ORIGINS = True
